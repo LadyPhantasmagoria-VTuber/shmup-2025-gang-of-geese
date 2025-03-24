d23c2a599436de3434bd844d19dc551241a91727
LadyPhantasmagoria-VTuber <nickyengelman@hotmail.com>
2025-03-24 10:45:34 +0100

    Menu script update

---
+++ Assets/Artists/William/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(0); // Load Game
+
+    }
+
+    //public void OnMenuButton()
+   // {
+       // SceneManager.LoadScene(); // Load Menu
+
+  //  }
+
+    //public void OnControlsButton()
+    //{
+    //    SceneManager.LoadScene(); // Load Controls
+    //}
+
+   public void OnQuitButton()
+   {
+      Application.Quit(); // Quit the game
+   }
+}
+
+//For additional scenes to switch between, copy paste a section and make sure the number in brackets alignes with the number in your scene manager (Look 
+//in the file -> Build profile -> Scene list to find out the corresponding number for the scenes)
